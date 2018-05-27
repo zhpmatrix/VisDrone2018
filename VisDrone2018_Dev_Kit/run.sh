5ec2b2b184a9a57bb681c8c2b538ec710030cfa5
zhpmatrix <zhpmatrix@gmail.com>
2018-05-27 22:11:47 +0800

    update run.sh

--- VisDrone2018_Dev_Kit/run.sh
+++ VisDrone2018_Dev_Kit/run.sh
@@ -3,7 +3,9 @@ source ~/.bashrc
 scp -r b418-xiwei@172.18.147.43:/home/b418-xiwei/zhanghaipeng/faster-rcnn.pytorch-master/annotations/faster_rcnn* /Users/zhpmatrix/Desktop/VisDrone/Results/Det/small_anchors/annotations
 
 MAXCHECKPOINT=3234
-for((EPOCH=5;EPOCH<=8;EPOCH++))
+START=8
+END=18
+for((EPOCH=$START;EPOCH<=$END;EPOCH++))
 do
 {
 ANNO_DIR=faster_rcnn_1_${EPOCH}_${MAXCHECKPOINT}.pth
