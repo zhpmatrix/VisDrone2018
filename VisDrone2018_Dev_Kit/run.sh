1d5e6e5f25f2bea2eae592201a8f32697229ce22
zhpmatrix <zhpmatrix@gmail.com>
2018-05-27 22:07:00 +0800

    update batch cmd

--- VisDrone2018_Dev_Kit/run.sh
+++ VisDrone2018_Dev_Kit/run.sh
@@ -1,9 +1,14 @@
+source ~/.bashrc
+
+scp -r b418-xiwei@172.18.147.43:/home/b418-xiwei/zhanghaipeng/faster-rcnn.pytorch-master/annotations/faster_rcnn* /Users/zhpmatrix/Desktop/VisDrone/Results/Det/small_anchors/annotations
+
 MAXCHECKPOINT=3234
-for((EPOCH=10;EPOCH<=10;EPOCH++))
+for((EPOCH=5;EPOCH<=8;EPOCH++))
 do
 {
 ANNO_DIR=faster_rcnn_1_${EPOCH}_${MAXCHECKPOINT}.pth
 matlab -nodesktop -r "anno_dir='$ANNO_DIR'",evalDET
 }&
 done
+wait
 echo "Done!"
